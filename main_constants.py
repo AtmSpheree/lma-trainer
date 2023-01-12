a5eabb67b10380eb0692588f860e8873076d3729
Grigorii Kondrashov <59977105+AtmSpheree@users.noreply.github.com>
2023-01-12 23:59:02 +0300

    An error was fixed due to an incorrect directory
    WORKING_DIRECTORY caused an error when compiling the program.
    WORKING_DIRECTORY has been removed.
    The structure of the paths functions without it.

--- main_constants.py
+++ main_constants.py
@@ -1,25 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from pathlib import Path
 from os import path
 import pygame
-
-# Working Directory Path
-WORKING_DIRECTORY = Path(__file__).parent
 
 # Main Screen Display Settings
 SCREEN_SIZE = SCREEN_WIDTH, SCREEN_HEIGHT = 1200, 800
 FPS = 50
 
 # Fonts
-FONT_PATH_INTER_LIGHT = path.join(WORKING_DIRECTORY,
-                                  'fonts/Inter-Light.ttf')
-FONT_PATH_INTER_REGULAR = path.join(WORKING_DIRECTORY,
-                                    'fonts/Inter-Regular.ttf')
-FONT_PATH_INTER_ITALIC = path.join(WORKING_DIRECTORY,
-                                   'fonts/Inter-Italic.ttf')
-FONT_PATH_INTER_EXTRABOLD = path.join(WORKING_DIRECTORY,
-                                      'fonts/Inter-ExtraBold.ttf')
+FONT_PATH_INTER_LIGHT = path.join('fonts/Inter-Light.ttf')
+FONT_PATH_INTER_REGULAR = path.join('fonts/Inter-Regular.ttf')
+FONT_PATH_INTER_ITALIC = path.join('fonts/Inter-Italic.ttf')
+FONT_PATH_INTER_EXTRABOLD = path.join('fonts/Inter-ExtraBold.ttf')
 
 # Main Screen Text
 TEXT_MAIN_SCREEN = {'back_button': {'strings': ['Вернуться',
@@ -55,25 +47,34 @@ TEXT_LEVEL_SELECTION_SCREEN = {'main_text': {'strings': ['Выберите ур�
                                                      'color': pygame.Color('white')}}
 
 # Main Screen Images
-IMAGE_MAIN_SCREEN = {'back_button': path.join(WORKING_DIRECTORY,
-                                              'data/icons/turn-back.png'),
-                     'exit_button': path.join(WORKING_DIRECTORY,
-                                              'data/icons/exit.png')}
+IMAGE_MAIN_SCREEN = {'back_button': path.join('data/icons/turn-back.png'),
+                     'exit_button': path.join('data/icons/exit.png')}
 
 # Main Colors
 COLOR_INTRODUCTION_DESIGN = '#E1CC4F'
 COLOR_GAME_DESIGN = '#D0C3DE'
 
 # Images
-IMAGE_BEE_1 = path.join(WORKING_DIRECTORY,
-                        'data/images/bee_1.png')
-IMAGE_BEE_2 = path.join(WORKING_DIRECTORY,
-                        'data/images/bee_2.png')
-IMAGE_BEE_3 = path.join(WORKING_DIRECTORY,
-                        'data/images/bee_3.png')
-IMAGE_BEE_4 = path.join(WORKING_DIRECTORY,
-                        'data/images/bee_4.png')
-IMAGE_OUR_TEAM_PHOTO = path.join(WORKING_DIRECTORY,
-                                 'data/images/our_team_image.png')
-IMAGE_ARROW_1 = path.join(WORKING_DIRECTORY,
-                          'data/icons/arrow_1.png')
+IMAGE_BEE_1 = path.join('data/images/bee_1.png')
+IMAGE_BEE_2 = path.join('data/images/bee_2.png')
+IMAGE_BEE_3 = path.join('data/images/bee_3.png')
+IMAGE_BEE_4 = path.join('data/images/bee_4.png')
+IMAGE_OUR_TEAM_PHOTO = path.join('data/images/our_team_image.png')
+IMAGE_ARROW_1 = path.join('data/icons/arrow_1.png')
+
+# Games variables
+# Colors Game Medium Difficulty
+DATA_COLORS_GAME_MEDIUM_DIFF_COLORS = {'Красный': '#DA1135',
+                                       'Синий': '#0995E3',
+                                       'Жёлтый': '#DECC2A',
+                                       'Зелёный': '#29B537'}
+DATA_COLORS_GAME_MEDIUM_DIFF_FIELD_SIZE = (3, 5)
+# Colors Game Hard Difficulty
+DATA_COLORS_GAME_HARD_DIFF = {'Красный': '#DA1135',
+                              'Синий': '#0995E3',
+                              'Жёлтый': '#DECC2A',
+                              'Зелёный': '#29B537',
+                              'Фиолетовый': '##794BDC',
+                              'Белый': '#f0f0f0',
+                              'Оранжевый': '#f80'}
+DATA_COLORS_GAME_HARD_DIFF_FIELD_SIZE = (4, 6)
