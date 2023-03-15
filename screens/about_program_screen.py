300da5e766ba5a48eb26258101ba2ffe1f534f9d
Grigorii Kondrashov <59977105+AtmSpheree@users.noreply.github.com>
2023-03-15 23:33:26 +0300

    Adding about_program_screen information

--- screens/about_program_screen.py
+++ screens/about_program_screen.py
@@ -15,6 +15,40 @@ class AboutProgramScreen(MainScreenType):
         self.create_back_button()
 
     def init_design(self):
+        text = main_constants.TEXT_ABOUT_PROGRAM_SCREEN['main_text']
+        text = render_multiline_text(text['strings'],
+                                     text['size'], text['color'],
+                                     main_constants.FONT_PATH_INTER_REGULAR)
+        self.blit(text, ((main_constants.SCREEN_WIDTH - text.get_width()) // 2,
+                         20))
+        text = main_constants.TEXT_ABOUT_PROGRAM_SCREEN['description_text']
+        text = render_multiline_text(text['strings'],
+                                     text['size'], text['color'],
+                                     main_constants.FONT_PATH_INTER_LIGHT)
+        self.blit(text, ((main_constants.SCREEN_WIDTH - text.get_width()) // 2,
+                         140))
+        text = main_constants.TEXT_ABOUT_PROGRAM_SCREEN['exit_button']
+        text = render_multiline_text(text['strings'],
+                                     text['size'], text['color'],
+                                     main_constants.FONT_PATH_INTER_LIGHT)
+        self.blit(text, (300, 220))
+        text = main_constants.TEXT_ABOUT_PROGRAM_SCREEN['back_button']
+        text = render_multiline_text(text['strings'],
+                                     text['size'], text['color'],
+                                     main_constants.FONT_PATH_INTER_LIGHT)
+        self.blit(text, (300, 300))
+        text = main_constants.TEXT_ABOUT_PROGRAM_SCREEN['second_description_text']
+        text = render_multiline_text(text['strings'],
+                                     text['size'], text['color'],
+                                     main_constants.FONT_PATH_INTER_LIGHT)
+        self.blit(text, ((main_constants.SCREEN_WIDTH - text.get_width()) // 2,
+                         500))
+        image = main_objects.load_image(main_constants.IMAGE_MAIN_SCREEN['back_button'])
+        image = pygame.transform.smoothscale(image, (60, 60))
+        self.blit(image, (230, 300))
+        image = main_objects.load_image(main_constants.IMAGE_MAIN_SCREEN['exit_button'])
+        image = pygame.transform.smoothscale(image, (60, 60))
+        self.blit(image, (230, 220))
         # Creating backup
         self.backup = self.copy()
 
